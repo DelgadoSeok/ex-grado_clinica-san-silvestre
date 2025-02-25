b7ec88280d7ad5e0dc8c1647413118eca0ae46bf
Nayely Giles <nayelygilesvaldez@gmail.com>
2025-02-24 22:30:53 -0400

    [Feature] 🚀 Nuevos archivos y se actualizó app.py.

--- app/app.py
+++ app/app.py
@@ -21,6 +21,9 @@ from routes.deudas_secre_route import deudas_secre_bp
 from routes.egresos_secre_route import egresos_secre_bp
 from routes.pagos_secre_route import pagos_secre_bp
 
+# para clinica
+from routes.consultas_route import consultas_bp
+
 # inicializar aplicacion
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY", "supersecretkey123")  # Cambia esto por una clave segura
@@ -39,6 +42,10 @@ app.register_blueprint(egresos_bp)
 app.register_blueprint(deudas_secre_bp)
 app.register_blueprint(egresos_secre_bp)
 app.register_blueprint(pagos_secre_bp)
+
+# para clinica
+app.register_blueprint(consultas_bp)
+
 
 # Context processor para compartir el rol de usuario en todas las plantillas
 @app.context_processor
