0b1a675653767c4189589aef4c0731cedd3ad260
Nayely Giles <nayelygilesvaldez@gmail.com>
2025-02-28 00:01:48 -0400

    [Update] 🔥 Eliminando archivoos que no se usan

--- app/routes/deudas_secre_route.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-deudas_secre_bp = Blueprint('deudas_secre', __name__)
-
-@deudas_secre_bp.route('/deudas_secre')
-def contratos():
-    return render_template('views/deudas_secre.html')
--- app/services/referencias_service.py
+++
@@ -1,3 +0,0 @@
-from config.db import get_db_connection
-
-
